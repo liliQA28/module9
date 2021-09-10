ac462ac51b8e3092c897d5edd7cc9506e4b757ac
Liliana Quiroga <you@example.com>
2021-09-10 07:23:04 -0400

    commit Thursday

--- src/test/java/com/talent/SWApiTestWithRestAssured.java
+++ src/test/java/com/talent/SWApiTestWithRestAssured.java
@@ -1,22 +1,29 @@
 package com.talent;
 
+import com.talent.bind.BaseApiResponse;
 import io.restassured.RestAssured;
+import org.testng.annotations.Test;
+
+import javax.xml.bind.SchemaOutputResolver;
+
+import static io.restassured.config.LogConfig.logConfig;
+import static io.restassured.config.RestAssuredConfig.config;
 import static org.hamcrest.Matchers.*;
-import org.junit.jupiter.api.Test;
 
 public class SWApiTestWithRestAssured {
     @Test
-    public void requestAresourcesThenLinktoReturn (){
-        String body = RestAssured
+    public void whenRequestingAResourceThenLinksToResourcesMustBeReturned (){
+
+        RestAssured
                 .given()
-                .baseUri("https://swapi.dev/api")
-                .and()
-                .queryParam("format","json")
-                .when()
-                .get("/")
-                .then()
-                .log().all()
-                .and().assertThat().statusCode(is(equalTo(200)))
+                .queryParam("format", "json")
+                .config(config().logConfig(logConfig()
+                        .enableLoggingOfRequestAndResponseIfValidationFails()))
+                .when().get("https://swapi.dev/api/planets/?format=json")
+                .then().assertThat()
+                .statusCode(is(equalTo(200)));
+
+
 
     }
 }
---
+++ src/test/java/com/talent/bind/BaseApiResponse.java
@@ -0,0 +1,35 @@
+package com.talent.bind;
+
+   public class BaseApiResponse {
+        private String films;
+        private String vehicles;
+        private String people;
+        private String starships;
+        private String species;
+        private String planets;
+
+        public String getFilms() {
+            return films;
+        }
+
+        public String getVehicles() {
+            return vehicles;
+        }
+
+        public String getPeople() {
+            return people;
+        }
+
+        public String getStarships() {
+            return starships;
+        }
+
+        public String getSpecies() {
+            return species;
+        }
+
+        public String getPlanets() {
+            return planets;
+        }
+    }
+
